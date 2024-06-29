0cdec3e0628d9da5a71f7d728c5973088faa1135
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2024-06-29 07:52:48 -0700

    chore: Remove copyright headers from source files
    Copyright headers are not required when distributing public domain sample code.

--- src/hooks/prerun-hello.ts
+++ src/hooks/prerun-hello.ts
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2023, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
-
 import { type Hook } from '@oclif/core';
 
 // eslint-disable-next-line @typescript-eslint/require-await
